0086c9ff626f8228807438d1373c4b14974b239e
Сергей <aikon@bk.ru>
2025-09-24 00:26:41 +0500

    sb/Add task of codewars - 4

--- codewars2.py
+++ codewars2.py
@@ -4,6 +4,6 @@ def validate_base(num: str, base: int) -> bool:
             return False
     return True
 gggh = 54
-num = 'ABdshuhardsgjCDEfgdknfjnrtnF'#fgdhgh
+num = 'ABdshuhrdsgjCDEfgdknfjnrtnF'#fgdhgh
 base = 16
 print(validate_base(num, base))
