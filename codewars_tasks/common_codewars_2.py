1446b8eb99c3a21f43cc2f75956c16c95ec9cd41
Olga-Sd <olgafiles@mail.ru>
2025-09-25 19:01:18 +0300

    os/25.09 19:00 Добавлено решение задачи common_codewars_2.py

--- codewars_tasks/common_codewars_2.py
+++ codewars_tasks/common_codewars_2.py
@@ -6,6 +6,15 @@
 def remove_every_other(my_list):
     pass
 
+def remove_every_other_Olga(my_list):
+    i = 1
+    n = len(my_list)
+    while i < n:
+        my_list.pop(i)
+        i += 1
+        n -= 1
+    return my_list
+
 arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 print(remove_every_other(arr))
