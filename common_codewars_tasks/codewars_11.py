2785e67e0c934480d4347a35934b260aa8005495
Olga-Sd <olgafiles@mail.ru>
2025-10-03 22:18:27 +0300

    OS/03.10 22:20 Codewars_11.py solution by Olga is added

--- common_codewars_tasks/codewars_11.py
+++ common_codewars_tasks/codewars_11.py
@@ -12,3 +12,18 @@
 #
 # Find the unique number (this kata)
 # Find the unique string
+
+def find_uniq(arr):
+    if arr[0] != arr[1]:
+        if arr[1] == arr[2]:
+            return arr[0]
+        else:
+            return arr[1]
+    for i in range(2, len(arr)):
+        if arr[i-1] != arr[i]:
+            return arr[i]
+
+
+arr = [1, 1, 1, 2, 1, 1, 1, 1]
+print(find_uniq(arr))
+
